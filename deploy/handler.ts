d66eadd10fadc98ee0b9f7702652d6156e39e7aa
danielsss <better.sunjian@gmail.com>
2023-02-20 22:20:05 +1100

    added: router

--- deploy/handler.ts
+++ deploy/handler.ts
@@ -1,9 +1,9 @@
 import { serve } from 'https://deno.land/std@0.140.0/http/server.ts';
-import * as path from "https://deno.land/std@0.177.0/node/path.ts";
-import * as fs from "https://deno.land/std@0.177.0/node/fs.ts";
+import * as path from 'https://deno.land/std@0.177.0/node/path.ts';
+import * as fs from 'https://deno.land/std@0.177.0/node/fs.ts';
+import { router } from 'https://crux.land/router@0.0.5';
 
-
-const handleRequest = async function (request: Request): Promise<Response> {
+const treeHandler = async function (request: Request): Promise<Response> {
   const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
   const tree = path.join(__dirname, '../docs/transcript_tree.html');
   return new Response(fs.readFileSync(tree, 'utf-8'), {
@@ -11,5 +11,15 @@ const handleRequest = async function (request: Request): Promise<Response> {
   });
 }
 
+const memoryHandler = async function () {
+  return new Response(JSON.stringify(Deno.memoryUsage(), 2, null), {
+    headers: { 'content-type': 'application/json; charset=utf-8' }
+  });
+}
 
-serve('/transcript/tree.html', handleRequest);
+const handler = router({
+  'GET@/memory': memoryHandler,
+  'GET@/transcript/tree.html': treeHandler,
+});
+
+serve(handler);
