1672ff3229e6fb3cdf2b43bd57f05ecd31fdf684
danielsss <better.sunjian@gmail.com>
2023-02-20 22:28:38 +1100

    fixed: bugs

--- deploy/handler.ts
+++ deploy/handler.ts
@@ -1,12 +1,13 @@
 import { serve } from 'https://deno.land/std@0.140.0/http/server.ts';
 import * as path from 'https://deno.land/std@0.177.0/node/path.ts';
-import * as fs from 'https://deno.land/std@0.177.0/node/fs.ts';
 import { router } from 'https://crux.land/router@0.0.5';
 
 const treeHandler = async function (request: Request): Promise<Response> {
   const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
   const tree = path.join(__dirname, '../docs/transcript_tree.html');
-  return new Response(fs.readFileSync(tree, 'utf-8'), {
+  const decoder = new TextDecoder('utf-8');
+  const contents = await Deno.readFile(tree);
+  return new Response(decoder.decode(contents), {
     headers: { 'content-type': 'text/html; charset=utf-8' }
   });
 }
