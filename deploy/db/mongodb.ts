c116cdd379d67d48e21bb2216fc89c8ffa78fe16
danielsss <better.sunjian@gmail.com>
2023-02-22 10:26:06 +1100

    updated: mongo connect opts

--- deploy/db/mongodb.ts
+++ deploy/db/mongodb.ts
@@ -1,10 +1,23 @@
 import { MongoClient } from 'https://deno.land/x/mongo@v0.31.1/mod.ts';
 
-const MONGO_DB_URL = Deno.env.get('MONGO_DB_URL');
-
 try {
   const client = new MongoClient();
-  await client.connect(MONGO_DB_URL, { useNewUrlParser: true });
+  await client.connect({
+    db: Deno.env.get('MONGODB_DATABASE'),
+    tls: true,
+    servers: [
+      {
+        host: Deno.env.get('MONGODB_HOST'),
+        port: Deno.env.get('MONGODB_PORT') || 27017,
+      },
+    ],
+    credential: {
+      username: Deno.env.get('MONGODB_USERNAME'),
+      password: Deno.env.get('MONGODB_PASSWORD'),
+      db: Deno.env.get('MONGODB_DATABASE'),
+      mechanism: 'SCRAM-SHA-1',
+    }
+  });
 } catch (err) {
   console.error('Error connecting to MongoDB', err);
   throw err;
